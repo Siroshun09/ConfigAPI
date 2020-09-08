49b91069e298052e90a81dda77e8729e17a83577
Siroshun09 <siro.shun8@gmail.com>
2020-09-09 04:13:09 +0900

    [Change] IO に関係する部分は Exception を投げるようにする

--- common/src/main/java/com/github/siroshun09/configapi/common/FileUtils.java
+++ common/src/main/java/com/github/siroshun09/configapi/common/FileUtils.java
@@ -3,6 +3,7 @@ package com.github.siroshun09.configapi.common;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
+import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Objects;
@@ -61,5 +62,83 @@ public final class FileUtils {
             }
         }
         return Files.isRegularFile(path) && Files.isReadable(path) && Files.isWritable(path);
+    }
+
+    /**
+     * If the file does not exist in the specified path, creates its parent directory and it.
+     *
+     * @param path file path
+     * @return given file path
+     * @throws IOException          if an I/O error occurs
+     * @throws NullPointerException if the path is null
+     */
+    public static Path createFileIfNotExists(@NotNull Path path) throws IOException {
+        Objects.requireNonNull(path, "path");
+
+        if (!Files.exists(path)) {
+            Files.createFile(path);
+        }
+
+        return path;
+    }
+
+    /**
+     * If the directory does not exist in the specified path, creates its parent directory and it.
+     *
+     * @param path directory path
+     * @return given file path
+     * @throws IOException          if an I/O error occurs
+     * @throws NullPointerException if the path is null
+     */
+    public static Path createDirectoriesIfNotExists(@NotNull Path path) throws IOException {
+        Objects.requireNonNull(path, "path");
+
+        if (!Files.exists(path) || (!Files.isDirectory(path) && !Files.isSymbolicLink(path))) {
+            Files.createDirectories(path);
+        }
+
+        return path;
+    }
+
+    /**
+     * Deletes the file.
+     *
+     * @param path file path
+     * @throws IOException          if an I/O error occurs
+     * @throws NullPointerException if the path is null
+     */
+    public static void deleteFile(@NotNull Path path) throws IOException {
+        Objects.requireNonNull(path, "path");
+
+        if (Files.exists(path) && Files.isRegularFile(path)) {
+            Files.delete(path);
+        }
+    }
+
+    /**
+     * Deletes the given directory and all directories and files under it.
+     *
+     * @param path given directory path
+     * @throws IOException          if an I/O error occurs
+     * @throws NullPointerException if the path is null
+     */
+    public static void deleteDirectory(@NotNull Path path) throws IOException {
+        Objects.requireNonNull(path, "path");
+
+        if (!Files.exists(path) || !Files.isDirectory(path)) {
+            return;
+        }
+
+        try (DirectoryStream<Path> contents = Files.newDirectoryStream(path)) {
+            for (Path file : contents) {
+                if (Files.isDirectory(file)) {
+                    deleteDirectory(file);
+                } else {
+                    Files.delete(file);
+                }
+            }
+        }
+
+        Files.delete(path);
     }
 }
