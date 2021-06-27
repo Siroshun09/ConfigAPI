2a3ff45470304535d477be63b7deadd56ae9468d
Siroshun09 <siro.shun8@gmail.com>
2021-06-28 07:17:31 +0900

    feat: add ConfigurationSerializer

---
+++ api/src/main/java/com/github/siroshun09/configapi/api/serializer/ConfigurationSerializer.java
@@ -0,0 +1,44 @@
+/*
+ *     Copyright 2021 Siroshun09
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+
+package com.github.siroshun09.configapi.api.serializer;
+
+import com.github.siroshun09.configapi.api.Configuration;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * An interface that converts an object to a {@link Configuration}.
+ *
+ * @param <T> the type of value
+ */
+public interface ConfigurationSerializer<T> {
+
+    /**
+     * Serializes the value.
+     *
+     * @param value the value to serialize
+     * @return the {@link Configuration}
+     */
+    @NotNull Configuration serialize(@NotNull T value);
+
+    /**
+     * Deserializes the value.
+     *
+     * @param source the source {@link Configuration}
+     * @return the deserialized value
+     */
+    @NotNull T deserialize(@NotNull Configuration source);
+}
